bc20c1df35b7fdb5d9ca7af111851e41304829e8
Life is squid <rredzer000@gmail.com>
2024-02-21 19:07:51 +0000

    added Azure Api link to Blazor

--- BlazorClientApp/Data/ApiData.cs
+++ BlazorClientApp/Data/ApiData.cs
@@ -3,5 +3,6 @@
     public static class ApiData
     {
         public static readonly string ApiLink = "https://localhost:7153/";
+        public static readonly string AzureApiLink = "https://aspwebapiblob.azurewebsites.net/";
     }
 }
