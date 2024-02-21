aa3f14fd6ab9244291f6971405d1595150a5b9d0
Life is squid <rredzer000@gmail.com>
2024-02-21 18:45:19 +0000

    Azure fucntion test DI update

--- ApiTest/BlobAzureFunctionTest.cs
+++ ApiTest/BlobAzureFunctionTest.cs
@@ -1,65 +1,90 @@
-﻿//using FakeItEasy;
-//using FunctionApp;
+﻿using Application.Core;
+using FakeItEasy;
+using FunctionApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
-//namespace ApiTest
-//{
-//    public class BlobAzureFunctionTest
-//    {
-//        [Theory]
-//        [InlineData("tes", "link", "name")]
-//        [InlineData("tes@c.com", null, "name")]
-//        [InlineData(null, null, null)]
-//        public async Task FunctionRunFail(string email, string fileLink, string name)
-//        {
-//            EmailNotificationFunction emailNotificationFunction = new EmailNotificationFunction();
+namespace ApiTest
+{
+    public class BlobAzureFunctionTest
+    {
+        private readonly IOptions<SmtpData> _emailOptions;
 
-//            var stream = A.Fake<Stream>();
-//            Dictionary<string, string> meta = new Dictionary<string, string>();
-//            meta.Add("email", email);
-//            meta.Add("fileLink", fileLink);
+        //TODO
+        public BlobAzureFunctionTest()
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<EmailServiceTest>()
+            .Build();
 
-//            bool funcResult = true;
-//            try
-//            {
-//                funcResult = await emailNotificationFunction.Run(stream, name, meta);
-//            }
-//            catch (Exception ex)
-//            {
-//                funcResult = false;
-//            }
-//            finally
-//            {
-//                Assert.False(funcResult);
-//            }
-//        }
+            SmtpSecutiry smtpSecurity = new SmtpSecutiry();
+            configuration.GetSection("SmtpGmailSecurity").Bind(smtpSecurity);
 
-//        [Theory]
-//        [InlineData("existingmail1@mail.com", "link1", "")]
-//        [InlineData("existingmail2@mail.com", "link2", "")]
-//        public async Task FunctionRunSuccess(string email, string fileLink, string name)
-//        {
-//            EmailNotificationFunction emailNotificationFunction = new EmailNotificationFunction();
+            var emailOptions = new SmtpData()
+            {
+                SMTPServer = smtpSecurity.SMTPServer,
+                Port = smtpSecurity.Port,
+                Login = smtpSecurity.Login,
+                Password = smtpSecurity.Password,
+            };
+            _emailOptions = Options.Create(emailOptions);
+        }
 
-//            var stream = A.Fake<Stream>();
-//            Dictionary<string, string> meta = new Dictionary<string, string>() 
-//            { 
-//                { "email", email},
-//                { "fileLink", fileLink}
-//            };
+        [Theory]
+        [InlineData("tes", "link", "name")]
+        [InlineData("tes@c.com", null, "name")]
+        [InlineData(null, null, null)]
+        public async Task FunctionRunFail(string email, string fileLink, string name)
+        {
+            EmailNotificationFunction emailNotificationFunction = new EmailNotificationFunction(_emailOptions);
 
-//            bool funcResult = true;
-//            try
-//            {
-//                funcResult = await emailNotificationFunction.Run(stream, name, meta);
-//            }
-//            catch (Exception ex)
-//            {
-//                funcResult = false;
-//            }
-//            finally
-//            {
-//                Assert.True(funcResult);
-//            }
-//        }
-//    }
-//}
+            var stream = A.Fake<Stream>();
+            Dictionary<string, string> meta = new Dictionary<string, string>();
+            meta.Add("email", email);
+            meta.Add("fileLink", fileLink);
+
+            bool funcResult = true;
+            try
+            {
+                funcResult = await emailNotificationFunction.Run(stream, name, meta);
+            }
+            catch (Exception ex)
+            {
+                funcResult = false;
+            }
+            finally
+            {
+                Assert.False(funcResult);
+            }
+        }
+
+        [Theory]
+        [InlineData("existingmail1@mail.com", "link1", "")]
+        [InlineData("existingmail2@mail.com", "link2", "")]
+        public async Task FunctionRunSuccess(string email, string fileLink, string name)
+        {
+            EmailNotificationFunction emailNotificationFunction = new EmailNotificationFunction(_emailOptions);
+
+            var stream = A.Fake<Stream>();
+            Dictionary<string, string> meta = new Dictionary<string, string>()
+            {
+                { "email", email},
+                { "fileLink", fileLink}
+            };
+
+            bool funcResult = true;
+            try
+            {
+                funcResult = await emailNotificationFunction.Run(stream, name, meta);
+            }
+            catch (Exception ex)
+            {
+                funcResult = false;
+            }
+            finally
+            {
+                Assert.True(funcResult);
+            }
+        }
+    }
+}
